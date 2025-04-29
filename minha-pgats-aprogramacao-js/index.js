ee69d59c27fc7dc5f8ad2e75512cecdfc9272111
angelapaulam <angelapaulamarques@gmail.com>
2025-04-29 13:06:52 -0300

    teste

--- minha-pgats-aprogramacao-js/index.js
+++ minha-pgats-aprogramacao-js/index.js
@@ -1,2 +1,19 @@
 
-console.log("texto angela novo")
+//console.log("texto angela novo")
+console.error("erro teste")
+
+const irmaos = ["thor", "hela"]
+console.table(irmaos)
+
+const irmaos = [
+  {
+    nome:"thor",
+    idade: 2
+  },
+  {
+    nome: "hela"
+    idade:3
+  }
+]
+console.table()
+
---
+++ minha-pgats-aprogramacao-js/tipos.js
@@ -0,0 +1,9 @@
+
+const numeroAula = "03"
+const turma = "02"
+let data = "05 de Abril"
+
+console.log("Aula 03 da Turma " + turma + " no Sábado dia 05 de Abril")
+
+console.log(`Aula ${ numeroAula } da Turma ${ turma } no Sábado dia ${ data }`)
+
