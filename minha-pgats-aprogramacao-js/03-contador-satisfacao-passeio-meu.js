251382cb4b0c59e21a360db60879689dc3d22c92
angelapaulam <angelapaulamarques@gmail.com>
2025-05-21 21:38:15 -0300

    first commit

---
+++ minha-pgats-aprogramacao-js/03-contador-satisfacao-passeio-meu.js
@@ -0,0 +1,24 @@
+/**
+
+*/
+
+let quantidadeDeVoltas = 5
+let indice = 1
+
+for (let indice = 1; indice <= quantidadeDeVoltas; indice++){
+    //acao de dar voltas
+    console.log(`Numero da volta atual: ${quantidadeDeVoltas}`)
+    if (quantidadeDeVoltas >= 5){
+      console.log(`O dog esta satisfeito, ja deu: ${quantidadeDeVoltas} voltas`)
+    }
+}
+
+let voltasParaDogFicarSatisfeito = 5
+let volta = 1
+while(volta <= voltasParaDogFicarSatisfeito){
+  console.log(`Volta de nº ${volta} com o Dog`)
+}
+volta++
+
+
+
