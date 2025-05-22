0d1ef50b292d3ba4f5519b95686ce8f723440e08
angelapaulam <angelapaulamarques@gmail.com>
2025-05-22 12:11:28 -0300

    altera

--- minha-pgats-aprogramacao-js/03-contador-satisfacao-passeio-meu.js
+++ minha-pgats-aprogramacao-js/03-contador-satisfacao-passeio-meu.js
@@ -7,18 +7,23 @@ let indice = 1
 
 for (let indice = 1; indice <= quantidadeDeVoltas; indice++){
     //acao de dar voltas
-    console.log(`Numero da volta atual: ${quantidadeDeVoltas}`)
-    if (quantidadeDeVoltas >= 5){
-      console.log(`O dog esta satisfeito, ja deu: ${quantidadeDeVoltas} voltas`)
-    }
+    console.log(`Numero da volta atual: ${indice}`) 
+    if (indice < 5){
+      console.log(`O dog não esta satisfeito, deu apenas: ${indice} voltas`)
+}
+
+}
+if (quantidadeDeVoltas = 5){
+  console.log(`O dog esta satisfeito, já deu: ${quantidadeDeVoltas} voltas`)
 }
 
 let voltasParaDogFicarSatisfeito = 5
 let volta = 1
 while(volta <= voltasParaDogFicarSatisfeito){
   console.log(`Volta de nº ${volta} com o Dog`)
+  volta++
 }
-volta++
-
-
+if (volta = 5){
+  console.log(`O dog esta satisfeito, já deu: ${volta} voltas`)
+}
 
