45b0d8b6fb2a8388e89562cef836f0fb6fcdad29
angelapaulam <angelapaulamarques@gmail.com>
2025-05-22 11:03:48 -0300

    exec

---
+++ minha-pgats-aprogramacao-js/exercicios/exercicio-nota1.js
@@ -0,0 +1,12 @@
+/**
+crie uma função em java script que pega uma lista de inteiros e strings e retorna uma nova lista sem as strings
+ */
+
+function filtrarApenasNumeros(lista) {
+  return lista.filter(item => typeof item === 'number');
+}
+
+const listaOriginal = [5, "a", 4, "n", 3, "gela", 2, 1];
+const listaSomenteNumeros = filtrarApenasNumeros(listaOriginal);
+
+console.log(listaSomenteNumeros); 
---
+++ minha-pgats-aprogramacao-js/exercicios/exercicio-nota2.js
@@ -0,0 +1,27 @@
+/**
+Retorne o número de vogais no texto fornecido. Consideraremos a, e, i, o e u como vogais para este Kata . O texto de entrada conterá apenas letras minúsculas e/ou espaços. Letras acentuadas não fazem parte desse desafio.  
+ */
+
+function contarVogais(texto) {
+  const vogais = 'aeiou';
+  let contador = 0;
+
+  for (let letra of texto) {
+    contador += vogais.includes(letra) ? 1 : 0;
+  }
+
+  return contador;
+}
+
+console.log(contarVogais('exercicio dois valendo nota na disciplina programacao para automacao de teste')); 
+// Saída: 6
+
+console.log(contarVogais('bom dia')); 
+// Saída: 3
+
+console.log(contarVogais(' ')); 
+// Saída: 0
+
+console.log(contarVogais('javascript')); 
+// Saída: 3
+
