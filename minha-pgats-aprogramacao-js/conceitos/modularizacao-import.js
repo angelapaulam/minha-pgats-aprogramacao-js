cc1c2bdf07fd89d9773b22d1cf7f4bc9e9f7ab07
angelapaulam <angelapaulamarques@gmail.com>
2025-05-26 21:37:44 -0300

    modularização

---
+++ minha-pgats-aprogramacao-js/conceitos/modularizacao-import.js
@@ -0,0 +1,9 @@
+//  export {} / import 
+
+// importar { o que queremos } de 'caminho do arquivo'
+import { BRINQUEDOS, exibirNomePet, exibirIdade } from './modularizacao-export.js'
+
+exibirNomePet(`Pipoca`)
+exibirNomePet(`angela`)
+exibirNomePet(BRINQUEDOS)
+exibirIdade(`49`)
